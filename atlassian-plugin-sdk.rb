6a0d652cd527ba4471b56cc735335f8b04f78106
lucasle <llephamngoc@atlassian.com>
2015-01-12 14:13:47 +0700

    Update atlassian-plugin-sdk.rb

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://maven.atlassian.com/repository/public/com/atlassian/amps/atlassian-plugin-sdk/5.0.11/atlassian-plugin-sdk-5.0.11.tar.gz'
-  sha1 'e067762c90245b246e51277d9f85e536dd4fc15b'
+  url 'https://maven.atlassian.com/repository/public/com/atlassian/amps/atlassian-plugin-sdk/5.0.13/atlassian-plugin-sdk-5.0.13.tar.gz'
+  sha1 '5bc50d7ddeae51ec3457afa6c3b0fa41f8de2a1c'
 
   def install
     # Remove windows files
