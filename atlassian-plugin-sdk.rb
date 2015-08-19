e44c4fb15992cbef1fb909654ca6c8db97cd32fa
Lucas Le <llephamngoc@atlassian.com>
2015-08-19 18:27:48 +0700

    Revert brew file

---
+++ atlassian-plugin-sdk.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class AtlassianPluginSdk < Formula
+  homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
+  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/5.1.1/atlassian-plugin-sdk-5.1.1.tar.gz'
+  sha1 'f432c707ccf091371e95c8dc1c2ff9bea4966a30'
+
+  def install
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
+
+    # Install jars in libexec to avoid conflicts
+    libexec.install Dir['*']
+
+    # Symlink binaries
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<-EOS.undent
+      *** Please note we have dropped support for Maven 2.x and Maven 3.0.x ***
+      Release notes can be found at https://developer.atlassian.com/display/DOCS/AMPS+SDK+5.0.0+Release+Notes
+      
+      Thanks for installing the Atlassian Plugin SDK. For more information,
+      visit https://developer.atlassian.com.
+
+      To create a plugin skeleton using atlas-create-APPLICATION-plugin, e.g.:
+        atlas-create-jira-plugin or atlas-create-confluence-plugin
+
+      To run your plugin's host application with the plugin skeleton installed:
+        atlas-run
+    EOS
+  end
+end
