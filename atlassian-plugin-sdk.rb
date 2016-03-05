50ee930391f558b6a3886331d4fac1e3b6d00175
bambooagent <build-agent@atlassian.com>
2016-03-05 22:18:18 +0000

    Update Atlassian plugin SDK version to 6.2.4

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/6.2.3/atlassian-plugin-sdk-6.2.3.tar.gz'
-  sha1 'b1999a8cadab2ceb06973a646a3c2f9be133c012'
+  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/6.2.4/atlassian-plugin-sdk-6.2.4.tar.gz'
+  sha1 '9d1f046f0cb83468f58544434f8d08e6fbfc7664'
 
   def install
     # Remove windows files
