16cdad92d48a362420eaf5f4ab4c7493db1d78c3
Nagasaka Takuya <takuya.nagasaka@gmail.com>
2016-08-19 21:05:16 +0900

    use sha256 checksum algorithms fix the issue #11

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -3,7 +3,7 @@ require 'formula'
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
   url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/6.2.9/atlassian-plugin-sdk-6.2.9.tar.gz'
-  sha1 '28ece032910b507d55eceaa7c80cd926dc2b76a1'
+  sha256 '0714f13943f857680f47491ff439d4b6134303e8f110c64a48294141f9710058'
 
   def install
     # Remove windows files
@@ -20,7 +20,7 @@ class AtlassianPluginSdk < Formula
     <<-EOS.undent
       *** Please note we have dropped support for Maven 2.x and Maven 3.0.x ***
       Release notes can be found at https://developer.atlassian.com/display/DOCS/AMPS+SDK+5.0.0+Release+Notes
-      
+
       Thanks for installing the Atlassian Plugin SDK. For more information,
       visit https://developer.atlassian.com.
 
