c63a2bd7c469a5fd6a4e662626ca6397ffd46a72
bambooagent <build-agent@atlassian.com>
2018-02-01 23:17:25 +0000

    Update Atlassian plugin SDK version to 6.3.7

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/6.3.6/atlassian-plugin-sdk-6.3.6.tar.gz'
+  url 'https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/6.3.7/atlassian-plugin-sdk-6.3.7.tar.gz'
   sha256 'be2211b8bc08ee6cc7019fd248d8135322274e6cb44269d00f17c601fb00ab46'  
 
   def install
