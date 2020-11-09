885fc8c2dfea3150ba98fc2c492582ce2e6b7b15
Chris Williams <cwilliams@atlassian.com>
2020-11-09 16:11:02 +1100

    Fixes according to homebrew linter

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -1,18 +1,19 @@
-require 'formula'
+# typed: false
+# frozen_string_literal: true
 
+# Atlassian Plugin SDK formula
 class AtlassianPluginSdk < Formula
-  desc "Atlassian Plugin SDK"
-  homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/8.2.6/atlassian-plugin-sdk-8.2.6.tar.gz'
-  sha256 '9423d0e27cbe81b16fc9909fc3ce1b1d784d44db567ad3c051974b2273a0f644'
-  version "8.2.6"
+  desc "Set of tools and dependencies for plugins on Atlassian server applications"
+  homepage "https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation"
+  url "https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/8.2.6/atlassian-plugin-sdk-8.2.6.tar.gz"
+  sha256 "9423d0e27cbe81b16fc9909fc3ce1b1d784d44db567ad3c051974b2273a0f644"
 
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
 
     # Install jars in libexec to avoid conflicts
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
 
     # Symlink binaries
     bin.install_symlink Dir["#{libexec}/bin/*"]
@@ -26,5 +27,9 @@ class AtlassianPluginSdk < Formula
       Release Notes can be found here:
       https://developer.atlassian.com/server/framework/atlassian-sdk/amps-sdk-release-notes/
     EOS
+  end
+
+  test do
+    system bin/"atlas-version"
   end
 end
