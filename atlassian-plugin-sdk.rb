88630718195fd9412ffee752dcb3a698cb25270b
fredrikattebrant <fredrik.attebrant@gmail.com>
2024-01-31 11:31:06 +0100

    Update to 8.2.9
    Also update the release notes URL.

--- atlassian-plugin-sdk.rb
+++ atlassian-plugin-sdk.rb
@@ -5,7 +5,7 @@
 class AtlassianPluginSdk < Formula
   desc "Set of tools and dependencies for plugins on Atlassian server applications"
   homepage "https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation"
-  url "https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/8.2.8/atlassian-plugin-sdk-8.2.8.tar.gz"
+  url "https://packages.atlassian.com/maven/public/com/atlassian/amps/atlassian-plugin-sdk/8.2.9/atlassian-plugin-sdk-8.2.9.tar.gz"
   sha256 "e6f3488542f657bdd0271f1c30508cf0c76f0162779463c06be78c0c432da51d"
 
   def install
@@ -25,7 +25,7 @@ class AtlassianPluginSdk < Formula
       visit https://developer.atlassian.com.
 
       Release Notes can be found here:
-      https://developer.atlassian.com/docs/amps-sdk-release-notes/amps-sdk-6-x-and-up-release-notes
+      https://developer.atlassian.com/server/framework/atlassian-sdk/amps-sdk-release-notes/
 
       To create a plugin skeleton using atlas-create-APPLICATION-plugin, e.g.:
         atlas-create-jira-plugin or atlas-create-confluence-plugin
