c9bdc3e67f9437c99a0a09bd16d50a0f05b34ed2
pglantschnig-appfire <paul.glantschnig@appfire.com>
2025-12-19 08:53:16 +0100

    added atlassian-plugin-sdk-9.1.2

---
+++ atlassian-plugin-sdk912.rb
@@ -0,0 +1,41 @@
+# typed: false
+# frozen_string_literal: true
+
+# Atlassian Plugin SDK formula
+class AtlassianPluginSdk912 < Formula
+  desc "Set of tools and dependencies for plugins on Atlassian server applications"
+  homepage "https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation"
+  url "https://packages.atlassian.com/mvn/maven-external/com/atlassian/amps/atlassian-plugin-sdk/9.1.2/atlassian-plugin-sdk-9.1.2.tar.gz"
+  sha256 "2f8caa8e0b5530575e54c5453e57b627b746eda39234c0462f1f64da268ca0e1"
+
+  def install
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
+
+    # Install jars in libexec to avoid conflicts
+    libexec.install Dir["*"]
+
+    # Symlink binaries
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<~EOS
+      Thanks for installing the Atlassian Plugin SDK. For more information,
+      visit https://developer.atlassian.com.
+
+      Release Notes can be found here:
+      https://developer.atlassian.com/server/framework/atlassian-sdk/amps-sdk-release-notes
+
+      To create a plugin skeleton using atlas-create-APPLICATION-plugin, e.g.:
+        atlas-create-jira-plugin or atlas-create-confluence-plugin
+
+      To run your plugin's host application with the plugin skeleton installed:
+        atlas-run
+    EOS
+  end
+
+  test do
+    system bin/"atlas-version"
+  end
+end
